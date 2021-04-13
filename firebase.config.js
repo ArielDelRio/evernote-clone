11f9590856a2111e0b16c861cd54beccdd33a340
ArieldRio <arieldelrioviamonte@gmail.com>
2021-04-13 09:00:32 -0500

    Add some packages for node js and set firebase config

---
+++ firebase.config.js
@@ -0,0 +1,7 @@
+const options = {
+    method: 'POST',
+    headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ id, course, lesson })
