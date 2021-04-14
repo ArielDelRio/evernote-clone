f22a4cd0cb9da5b3b8a7a6a5b8d241b716256386
ArieldRio <arieldelrioviamonte@gmail.com>
2021-04-14 15:33:40 -0500

    Remove unnecessary config file

--- firebase.config.js
+++
@@ -1,7 +0,0 @@
-const options = {
-    method: 'POST',
-    headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ id, course, lesson })
